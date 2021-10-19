b4c7b374bbf6cc407c86baad3404825519136b09
Joren <joren.hnns@gmail.com>
2021-10-19 19:08:13 +0200

    Add GLAD vendor library

--- game/src/tetris.cpp
+++ game/src/tetris.cpp
@@ -1,6 +1,18 @@
-#include <cstdio>
+#include <SDL.h>
 
 int main()
 {
-    printf("Hello, World!\n");
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        // TODO: Logging.
+        return 1;
+    }
+
+    SDL_CreateWindow("Tetris", 0, 0, 720, 480, SDL_WINDOW_OPENGL);
+
+    while (!SDL_QuitRequested())
+    {
+        
+    }
+
+    SDL_Quit();
 }
