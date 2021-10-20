6a5c2517774b4943e98e9456432cd8387cb95357
Joren <joren.hnns@gmail.com>
2021-10-20 19:32:13 +0200

    Set OpenGL context attributes

--- game/src/tetris.cpp
+++ game/src/tetris.cpp
@@ -10,6 +10,12 @@ int main()
         return 1;
     }
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS,
+        SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG | SDL_GL_CONTEXT_DEBUG_FLAG);
+
     SDL_Window* window = SDL_CreateWindow("Tetris", 0, 0, 720, 480, SDL_WINDOW_OPENGL);
     if (window == nullptr) {
         // TODO: Logging.
